f17dc55e23b0d0621557b40ec52e34ed7cf7fe6b
matheus <matheusfavaretto27@gmail.com>
2022-11-13 11:16:07 -0300

    adicionado mascara em telefone

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from TelefonesBr import TelefonesBr
 import re
 
-padrao = "\w{5,50}@\w{3,10}.\w{2,3}.\w{2,3}"
-texto = "aaabbbcc rodrigo123@gmail.com.br"
-resposta = re.search(padrao, texto)
+#padrao_molde = "(xx)aaaa-wwww"
+#padrao = "[0-9]{2}[0-9]{4,5}[0-9]{4}"
+#texto = "eu gosto do numero 2126451234"
+#resposta = re.search(padrao,texto)
 
-print(resposta.group())
+#print(resposta)
+telefone = "552126481234"
+telefone_objeto = TelefonesBr(telefone)
+
+print(telefone_objeto)
