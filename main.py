8879e4b7f558c917885b6e4961d72a9f7944542d
matheus <matheusfavaretto27@gmail.com>
2022-11-13 09:59:30 -0300

    adicionado Validação CNPJ

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from cpf import Cpf
+from cpf_cnpj import CpfCnpj
 from validate_docbr import CNPJ
 
-cnpj = CNPJ()
-exemplo_cnpj = "35379838000112"
-print(cnpj.validate(exemplo_cnpj))
+#cnpj = CNPJ()
+exemplo_cnpj = "353798380001112"
+#print(cnpj.validate(exemplo_cnpj))
 
 #cpf_um = Cpf(41400336813)
 #print(cpf_um)
-##
+documento = CpfCnpj(exemplo_cnpj, 'cnpj')
