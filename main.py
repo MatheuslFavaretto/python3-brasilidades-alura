1f95a48a64f52724416dabcee47db49410100b99
matheus <matheusfavaretto27@gmail.com>
2022-11-13 11:21:29 -0300

    adicionado datas

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-from TelefonesBr import TelefonesBr
-import re
+from datetime import datetime, timedelta
+from datasbr import DatasBr
 
-#padrao_molde = "(xx)aaaa-wwww"
-#padrao = "[0-9]{2}[0-9]{4,5}[0-9]{4}"
-#texto = "eu gosto do numero 2126451234"
-#resposta = re.search(padrao,texto)
-
-#print(resposta)
-telefone = "552126481234"
-telefone_objeto = TelefonesBr(telefone)
-
-print(telefone_objeto)
+cadastro = DatasBr()
+print(cadastro.momento_cadastro)
